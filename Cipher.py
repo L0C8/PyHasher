375bd01954a543825d475143a96cb92f44a80e56
L0C8 <TimB02@protonmail.com>
2025-03-11 14:29:40 +0100

    added AES encryption

--- Cipher.py
+++ Cipher.py
@@ -1,4 +1,7 @@
-import hashlib
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import hashlib, cryptography, base64, os
 
 def str_2_md5(text):
     md5_hash = hashlib.md5()
@@ -17,3 +20,35 @@ def file_2_md5(file_path):
         return "Error: File not found."
     except Exception as e:
         return f"Error: {e}"
+
+class AESCipher:
+    def __init__(self, key):
+        self.key = key  
+        self.backend = default_backend()
+
+    def encrypt(self, plaintext):
+        iv = os.urandom(16) 
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        encryptor = cipher.encryptor()
+
+        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+        padded_data = padder.update(plaintext.encode()) + padder.finalize()
+
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+        return base64.b64encode(iv + ciphertext).decode()
+
+    def decrypt(self, encrypted_text):
+        encrypted_data = base64.b64decode(encrypted_text)
+        iv = encrypted_data[:16]
+        ciphertext = encrypted_data[16:]
+
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        decryptor = cipher.decryptor()
+
+        decrypted_padded = decryptor.update(ciphertext) + decryptor.finalize()
+
+        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+        decrypted_data = unpadder.update(decrypted_padded) + unpadder.finalize()
+
+        return decrypted_data.decode()
