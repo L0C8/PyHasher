aee3307266d22ec260ea71f12e471f477a605d51
L0C8 <TimB02@protonmail.com>
2025-03-12 00:52:15 +0100

    added Custom TkInter library

--- Gui.py
+++ Gui.py
@@ -1,12 +1,16 @@
 import tkinter as tk
-import os
+import os, customtkinter
 from tkinter import ttk
 from Cipher import str_2_md5, str_2_sha1, str_2_sha256, file_2_md5, file_2_sha1, file_2_sha256 
 
+
+# ----- Cipher Functions -----
+
+# ----- Hash Functions ----- 
 def hash_text(event=None):  
     
     text = input_box.get()
-     
+    
     if file_mode.get():
         if not os.path.isfile(text):
             output_box.delete(0, tk.END)
@@ -34,7 +38,7 @@ def hash_text(event=None):
     output_box.delete(0, tk.END)
     output_box.insert(0, hashed)
 
-# Gui Setup
+# ----- Gui -----
 root = tk.Tk()
 root.title("PyHasher")
 root.geometry("480x256")
@@ -49,7 +53,7 @@ tab2 = ttk.Frame(notebook)
 notebook.add(tab1, text="Hasher")
 notebook.add(tab2, text="Cipher")
 
-# Widgets
+# ----- Tab 1 -----
 
 selected_hash = tk.StringVar(value="MD5")  
 
@@ -74,6 +78,20 @@ hash_button.pack(pady=5)
 output_box = ttk.Entry(tab1, width=50, state="normal")
 output_box.pack(pady=5)
 
-ttk.Label(tab2, text="Cipher").pack(pady=20)
+# ----- Tab 2 -----
+
+Cipher_Label_KeyInput = customtkinter.CTkLabel(
+    master=tab2,
+    text="Key",
+    font=("Arial", 14),
+    text_color="#000000",
+    height=30,
+    width=95,
+    corner_radius=0
+    )
+Cipher_Label_KeyInput.place(x=0, y=0)
+# input_box = ttk.Entry(tab2, width=50)
+# input_box.pack(pady=5)
+input_box.bind("<Return>", hash_text)  
 
 root.mainloop()
