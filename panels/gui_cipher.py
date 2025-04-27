02716f149b278fae41a58084f0cd86d9e8a7aa87
L0C8 <TimB02@protonmail.com>
2025-04-27 23:42:56 +0200

    added gui for hash

--- panels/gui_cipher.py
+++ panels/gui_cipher.py
@@ -1,1 +1,7 @@
-# gui cipher
+import tkinter as tk
+
+def create_cipher_tab(parent, theme):
+    frame = tk.Frame(parent, bg=theme['bg'])
+    label = tk.Label(frame, text="Cipher Panel", bg=theme['bg'], fg=theme['fg'])
+    label.pack(pady=20)
+    return frame
--- panels/gui_password.py
+++ panels/gui_password.py
@@ -1,1 +1,7 @@
-# gui password
+import tkinter as tk
+
+def create_password_tab(parent, theme):
+    frame = tk.Frame(parent, bg=theme['bg'])
+    label = tk.Label(frame, text="Password Panel", bg=theme['bg'], fg=theme['fg'])
+    label.pack(pady=20)
+    return frame
