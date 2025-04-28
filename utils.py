a9a00dc3d951c11b63062e6cb9628404cc146dea
L0C8 <TimB02@protonmail.com>
2025-04-28 19:30:11 +0200

    added theme to settings (temp)

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
-# PyHasher Utils
+import configparser
+import os
+
+def save_selected_theme(theme_name):
+    config = configparser.ConfigParser()
+    config['Settings'] = {'theme': theme_name}
+    with open('data/settings.ini', 'w') as configfile:
+        config.write(configfile)
+
+def load_selected_theme():
+    config = configparser.ConfigParser()
+    if os.path.exists('data/settings.ini'):
+        config.read('data/settings.ini')
+        return config.get('Settings', 'theme', fallback='Dark')
+    return 'Dark'
